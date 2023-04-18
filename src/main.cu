5a02d05ad6d99df4976bd40b57bf02d0b9a20af8
Adam Smelko <adamsmelko1@gmail.com>
2023-04-18 11:21:40 +0200

    try soft and hard transition points

--- src/main.cu
+++ src/main.cu
@@ -11,6 +11,7 @@
 #include <thrust/sort.h>
 
 #include "simulation.h"
+#include "timer.h"
 
 void cuda_check(cudaError_t e, const char* file, int line)
 {
@@ -27,6 +28,11 @@ void statistics_windows_probs(std::vector<std::map<size_t, float>>& probs, float
 							  thrust::device_ptr<size_t> traj_states, thrust::device_ptr<float> traj_times,
 							  size_t max_traj_len, size_t n_trajectories)
 {
+	timer t;
+	long long slices_time = 0.f, partition_time = 0.f, sort_time = 0.f, reduce_time = 0.f, update_time = 0.f;
+
+	t.start();
+
 	// sice traj_times does not contain time slices, but the timepoints of transitions,
 	// we compute a beginning for each timepoint for convenience
 	thrust::device_vector<float> traj_time_starts(n_trajectories * max_traj_len);
@@ -34,9 +40,15 @@ void statistics_windows_probs(std::vector<std::map<size_t, float>>& probs, float
 	thrust::transform(traj_times, traj_times + n_trajectories * max_traj_len, traj_time_starts.begin(),
 					  traj_time_starts.begin(), thrust::minus<float>());
 
+	t.stop();
+
+	slices_time = t.millisecs();
+
 	// begin and end of the whole traj batch
 	auto begin = thrust::make_zip_iterator(traj_states, traj_time_starts.begin(), traj_times);
 	auto end = begin + n_trajectories * max_traj_len;
+
+	auto hard_partition_point = begin;
 
 	thrust::device_vector<size_t> d_res_states;
 	thrust::device_vector<float> d_res_times;
@@ -46,19 +58,33 @@ void statistics_windows_probs(std::vector<std::map<size_t, float>>& probs, float
 
 	// we process a time window at a time
 	size_t window_idx = 0;
-	for (float cumul_time = window_size; cumul_time < max_time; cumul_time += window_size, window_idx++)
+	for (float cumul_time = window_size; cumul_time < max_time;
+		 cumul_time += window_size, window_idx++, begin = hard_partition_point)
 	{
 		float w_b = cumul_time - window_size;
 		float w_e = cumul_time;
 
-		// find states in the window by moving them to the front
-		auto partition_point =
+		t.start();
+
+		hard_partition_point =
 			thrust::partition(begin, end, [w_b, w_e] __device__(const thrust::tuple<size_t, float, float>& t) {
-				const float b = thrust::get<1>(t);
 				const float e = thrust::get<2>(t);
 
-				return !(e < w_b || b >= w_e);
+				return e <= w_e;
 			});
+
+		// find states in the window by moving them to the front
+		auto partition_point = thrust::partition(hard_partition_point, end,
+												 [w_b, w_e] __device__(const thrust::tuple<size_t, float, float>& t) {
+													 const float b = thrust::get<1>(t);
+													 const float e = thrust::get<2>(t);
+
+													 return !(e < w_b || b >= w_e);
+												 });
+
+		t.stop();
+
+		partition_time += t.millisecs();
 
 		size_t states_in_window_size = partition_point - begin;
 
@@ -66,28 +92,44 @@ void statistics_windows_probs(std::vector<std::map<size_t, float>>& probs, float
 		if (states_in_window_size == 0)
 			continue;
 
+		t.start();
+
 		// let us sort (state, (time_b, time_e)) array by state so we can reduce it in the next step
-		thrust::sort_by_key(traj_states, traj_states + states_in_window_size,
-							thrust::make_zip_iterator(traj_time_starts.begin(), traj_times));
+		thrust::sort_by_key(
+			begin.get_iterator_tuple().get<0>(), begin.get_iterator_tuple().get<0>() + states_in_window_size,
+			thrust::make_zip_iterator(begin.get_iterator_tuple().get<1>(), begin.get_iterator_tuple().get<2>()));
+
+		t.stop();
+
+		sort_time += t.millisecs();
 
 		// create transform iterator, which computes the intersection of the window and the transition time slice
-		auto time_slices_begin =
-			thrust::make_transform_iterator(thrust::make_zip_iterator(traj_time_starts.begin(), traj_times),
-											[w_b, w_e] __host__ __device__(const thrust::tuple<float, float>& t) {
-												const float b = thrust::get<0>(t);
-												const float e = thrust::get<1>(t);
-												return fminf(w_e, e) - fmaxf(w_b, b);
-											});
+		auto time_slices_begin = thrust::make_transform_iterator(
+			thrust::make_zip_iterator(begin.get_iterator_tuple().get<1>(), begin.get_iterator_tuple().get<2>()),
+			[w_b, w_e] __host__ __device__(const thrust::tuple<float, float>& t) {
+				const float b = thrust::get<0>(t);
+				const float e = thrust::get<1>(t);
+				return fminf(w_e, e) - fmaxf(w_b, b);
+			});
+
+		t.start();
 
 		d_res_states.resize(states_in_window_size);
 		d_res_times.resize(states_in_window_size);
 
 		// reduce sorted array of (state, time_slice)
 		// after this we have unique states in the first result array and sum of slices in the second result array
-		auto res_end = thrust::reduce_by_key(traj_states, traj_states + states_in_window_size, time_slices_begin,
-											 d_res_states.begin(), d_res_times.begin());
+		auto res_end = thrust::reduce_by_key(begin.get_iterator_tuple().get<0>(),
+											 begin.get_iterator_tuple().get<0>() + states_in_window_size,
+											 time_slices_begin, d_res_states.begin(), d_res_times.begin());
+
+		t.stop();
+
+		reduce_time += t.millisecs();
 
 		size_t res_size = res_end.first - d_res_states.begin();
+
+		t.start();
 
 		h_res_states.resize(res_size);
 		h_res_times.resize(res_size);
@@ -103,7 +145,18 @@ void statistics_windows_probs(std::vector<std::map<size_t, float>>& probs, float
 		{
 			probs[window_idx][h_res_states[i]] += h_res_times[i];
 		}
+
+		t.stop();
+
+		update_time += t.millisecs();
 	}
+
+	// print diagnostics
+	std::cout << "slices_time: " << slices_time << "ms" << std::endl;
+	std::cout << "partition_time: " << partition_time << "ms" << std::endl;
+	std::cout << "sort_time: " << sort_time << "ms" << std::endl;
+	std::cout << "reduce_time: " << reduce_time << "ms" << std::endl;
+	std::cout << "update_time: " << update_time << "ms" << std::endl;
 }
 
 int main()
@@ -164,16 +217,16 @@ int main()
 
 		trajectories = remaining_traj_end.get_iterator_tuple().get<0>() - d_states;
 
-		std::cout << "one sim " << trajectories << std::endl;
+		// std::cout << "one sim " << trajectories << std::endl;
 
-		for (size_t i = 0; i < probs.size(); ++i)
+		 for (size_t i = 0; i < probs.size(); ++i)
 		{
 			std::cout << "window " << i << std::endl;
 			for (auto& [state, time] : probs[i])
 			{
 				std::cout << state << " " << time / (1'000'000 * window_size) << std::endl;
 			}
-		}
+		 }
 	}
 
 	CUDA_CHECK(cudaFree(d_states));
