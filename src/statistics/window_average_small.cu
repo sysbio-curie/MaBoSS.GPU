b2f0030e4958f062c82d6f5a707d352f14ab5867
Adam Smelko <adamsmelko1@gmail.com>
2023-04-30 20:33:36 +0200

    add window_average_small

---
+++ src/statistics/window_average_small.cu
@@ -0,0 +1,188 @@
+#include <device_launch_parameters.h>
+
+#include <thrust/device_free.h>
+#include <thrust/device_malloc.h>
+
+#include "../cfg_config.h.generated"
+#include "../diagnostics.h"
+#include "../utils.h"
+#include "window_average_small.h"
+
+constexpr int non_internals = states_count - internals_count;
+constexpr int window_states = 1 << non_internals;
+
+__device__ int get_non_internal_index(const state_t& s)
+{
+	int idx = 0;
+
+	idx += s.is_set(6) ? 1 : 0;
+	idx += s.is_set(77) ? 2 : 0;
+	idx += s.is_set(103) ? 4 : 0;
+
+	return idx;
+}
+
+__global__ void window_average_small(int max_traj_len, int n_trajectories, float window_size,
+									 const state_t* __restrict__ traj_states, const float* __restrict__ traj_times,
+									 const float* __restrict__ traj_tr_entropies, float* __restrict__ window_probs,
+									 float* __restrict__ window_tr_entropies)
+{
+	auto id = blockIdx.x * blockDim.x + threadIdx.x;
+
+	if (id >= n_trajectories * max_traj_len)
+		return;
+
+	if (id % max_traj_len == 0 || traj_times[id] == 0.f)
+	{
+		return;
+	}
+
+	const int state_idx = get_non_internal_index(traj_states[id]);
+	const float tr_h = traj_tr_entropies[id];
+
+	float slice_begin = traj_times[id - 1];
+	float slice_end = traj_times[id];
+	int wnd_idx = floorf(slice_begin / window_size);
+
+	while (slice_end > slice_begin)
+	{
+		float wnd_end = (wnd_idx + 1) * window_size;
+		float slice_in_wnd = fminf(slice_end, wnd_end) - slice_begin;
+
+		atomicAdd(window_probs + (wnd_idx * window_states + state_idx), slice_in_wnd);
+		atomicAdd(window_tr_entropies + wnd_idx, tr_h * slice_in_wnd);
+
+		wnd_idx++;
+		slice_begin = fminf(slice_end, wnd_end);
+	}
+}
+
+window_average_small_stats::window_average_small_stats(float window_size, float max_time, state_t internal_mask,
+													   size_t max_traj_len, size_t max_n_trajectories)
+	: window_size_(window_size),
+	  max_time_(max_time),
+	  internal_mask_(internal_mask),
+	  max_traj_len_(max_traj_len),
+	  max_n_trajectories_(max_n_trajectories)
+{
+	timer t;
+	t.start();
+
+	size_t windows_count = std::ceil(max_time / window_size);
+
+	window_probs_ = thrust::device_malloc<float>(windows_count * window_states);
+	window_tr_entropies_ = thrust::device_malloc<float>(windows_count);
+
+	result_probs_.resize(windows_count * window_states);
+	result_tr_entropies_.resize(windows_count);
+
+	t.stop();
+
+	if (print_diags)
+	{
+		std::cout << "window_average_small> init_time: " << t.millisecs() << "ms" << std::endl;
+	}
+}
+
+window_average_small_stats::~window_average_small_stats()
+{
+	thrust::device_free(window_probs_);
+	thrust::device_free(window_tr_entropies_);
+}
+
+void window_average_small_stats::process_batch(thrust::device_ptr<state_t> traj_states,
+											   thrust::device_ptr<float> traj_times,
+											   thrust::device_ptr<float> traj_tr_entropies, int n_trajectories)
+{
+	timer t;
+	t.start();
+
+	window_average_small<<<DIV_UP(max_traj_len_ * n_trajectories, 512), 512>>>(
+		max_traj_len_, n_trajectories, window_size_, traj_states.get(), traj_times.get(), traj_tr_entropies.get(),
+		window_probs_.get(), window_tr_entropies_.get());
+
+	CUDA_CHECK(cudaDeviceSynchronize());
+
+	t.stop();
+
+	if (print_diags)
+	{
+		std::cout << "window_average_small> reduce_time: " << t.millisecs() << "ms" << std::endl;
+	}
+}
+
+void window_average_small_stats::finalize()
+{
+	size_t windows_count = std::ceil(max_time_ / window_size_);
+	timer t;
+
+	t.start();
+
+	// copy result data into host
+	CUDA_CHECK(cudaMemcpy(result_probs_.data(), thrust::raw_pointer_cast(window_probs_),
+						  windows_count * window_states * sizeof(float), cudaMemcpyDeviceToHost));
+	CUDA_CHECK(cudaMemcpy(result_tr_entropies_.data(), thrust::raw_pointer_cast(window_tr_entropies_),
+						  windows_count * sizeof(float), cudaMemcpyDeviceToHost));
+
+	t.stop();
+
+	if (print_diags)
+	{
+		std::cout << "window_average_small> finalize_time: " << t.millisecs() << "ms" << std::endl;
+	}
+}
+
+state_t non_internal_idx_to_state(const state_t& internal_mask, int idx)
+{
+	state_t ret;
+	size_t idx_i = 0;
+	for (size_t i = 0; i < states_count; i++)
+	{
+		if (!internal_mask.is_set(i))
+		{
+			if ((idx & (1 << idx_i)) != 0)
+				ret.set(i);
+			idx_i++;
+		}
+	}
+
+	return ret;
+}
+
+void window_average_small_stats::visualize(float window_size, int n_trajectories, const char* const* nodes)
+{
+	size_t windows_count = std::ceil(max_time_ / window_size_);
+
+	for (size_t i = 0; i < windows_count; ++i)
+	{
+		float entropy = 0.f;
+		float wnd_tr_entropy = result_tr_entropies_[i] / (n_trajectories * window_size);
+
+		for (size_t s_idx = 0; s_idx < window_states; s_idx++)
+		{
+			auto cumul_slices = result_probs_[i * window_states + s_idx];
+
+			if (cumul_slices == 0)
+				continue;
+
+			auto prob = cumul_slices / (n_trajectories * window_size);
+			entropy += -std::log2(prob) * prob;
+		}
+
+		std::cout << "window (" << i * window_size << ", " << (i + 1) * window_size << "]" << std::endl;
+		std::cout << "entropy: " << entropy << std::endl;
+		std::cout << "transition entropy: " << wnd_tr_entropy << std::endl;
+
+		for (size_t s_idx = 0; s_idx < window_states; s_idx++)
+		{
+			auto cumul_slices = result_probs_[i * window_states + s_idx];
+
+			if (cumul_slices == 0)
+				continue;
+
+			auto prob = cumul_slices / (n_trajectories * window_size);
+
+			std::cout << prob << " " << to_string(non_internal_idx_to_state(internal_mask_, s_idx), nodes) << std::endl;
+		}
+	}
+}
---
+++ src/statistics/window_average_small.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <map>
+#include <utility>
+#include <vector>
+
+#include <thrust/device_ptr.h>
+
+#include "../timer.h"
+#include "../types.h"
+
+class window_average_small_stats
+{
+	std::vector<float> result_probs_;
+	std::vector<float> result_tr_entropies_;
+
+	float window_size_;
+	float max_time_;
+	state_t internal_mask_;
+
+	size_t max_traj_len_;
+	size_t max_n_trajectories_;
+
+	thrust::device_ptr<float> window_probs_, window_tr_entropies_;
+
+public:
+	window_average_small_stats(float window_size, float max_time, state_t internal_mask, size_t max_traj_len,
+							   size_t max_n_trajectories);
+
+	~window_average_small_stats();
+
+	void process_batch(thrust::device_ptr<state_t> traj_states, thrust::device_ptr<float> traj_times,
+					   thrust::device_ptr<float> traj_tr_entropies, int n_trajectories_batch);
+
+	void finalize();
+
+	void visualize(float window_size, int n_trajectories, const char* const* nodes);
+};
