d79890caa8a6e5d3aca30d4eb3500e6b2a951213
Adam Smelko <adamsmelko1@gmail.com>
2023-11-25 13:22:29 +0100

    fixed_states to cub

--- src/statistics/fixed_states.h
+++ src/statistics/fixed_states.h
@@ -11,7 +11,17 @@ class fixed_states_stats : public stats
 	using result_t = std::map<static_state_t<state_words>, int>;
 	result_t result_;
 
+	size_t tmp_storage_bytes_ = 0;
+	void* d_tmp_storage_ = nullptr;
+	int* d_out_num;
+	static_state_t<state_words>* d_fixed_copy_ = nullptr;
+	static_state_t<state_words>* d_unique_states_ = nullptr;
+	int* d_unique_states_count_ = nullptr;
+
 public:
+	void initialize_temp_storage(thrust::device_ptr<static_state_t<state_words>> last_states,
+								 thrust::device_ptr<trajectory_status> traj_statuses, int n_trajectories);
+
 	void process_batch_internal(thrust::device_ptr<static_state_t<state_words>> last_states,
 								thrust::device_ptr<trajectory_status> traj_statuses, int n_trajectories);
 
