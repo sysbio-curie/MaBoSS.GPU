684930a442acd79866cbaf41bb1925d4db2e7a63
Adam Smelko <adamsmelko1@gmail.com>
2023-12-03 13:46:05 +0100

    fix generate noniternal index function

--- src/generator.cpp
+++ src/generator.cpp
@@ -217,7 +217,14 @@ void generator::generate_non_internal_index(std::ostringstream& os) const
 	{
 		if (!drv_.nodes[i].is_internal(drv_))
 		{
-			os << "((state[" << i / 32 << "] & " << (1u << i % 32) << "u) >> " << (i % 32) - non_internals++ << ")";
+			os << "((state[" << i / 32 << "] & " << (1u << i % 32) << "u)";
+			auto shift = (int)(i % 32) - non_internals++;
+			if (shift > 0)
+				os << " >> " << shift;
+			else if (shift < 0)
+				os << " << " << -shift;
+			os << ")";
+
 			if (non_internals != non_internals_count)
 			{
 				os << " | ";
